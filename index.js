e6e7636f288513a8bc3a1621bf49174d93019770
Zach Kaarvik <zkaarvik@gmail.com>
2020-12-04 21:14:34 -0800

    Turn off lights on TV play

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-// const bodyParser = require('body-parser');
 const fetch = require('node-fetch')
 const multer = require('multer');
 
@@ -8,7 +7,14 @@ const PORT = 3000
 
 const upload = multer({ dest: '/tmp/' });
 
-// app.use(bodyParser.json())
+const hueLocalApi = "http://192.168.0.107/api/"
+const apiKey = "6IzGK0uoAXNRNTAces76VHl2zzM6ojNpq6OK3NWA"
+const groups = "/groups/"
+const action = "/action"
+
+const livingRoomGroup = '1'
+const denGroup = '5'
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -17,10 +23,27 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 app.post('/plexwebhook',  upload.single('thumb'), (req, res, next) => {
   var payload = JSON.parse(req.body.payload);
   console.log('Got webhook for', payload.event);
-  // console.log(payload);
 
   if (payload.event == 'media.play' && payload.Player.title == '55" TCL Roku TV') {
     console.log('Media play event from the TV. Turning off the living room and den lights.')
+
+    var turnOffLightBody = {
+      'on': false
+    }
+
+    //Turn off living room lights
+    fetch(hueLocalApi + apiKey + groups + livingRoomGroup + action, {
+      method: 'put',
+      body: JSON.stringify(turnOffLightBody),
+      headers: { 'Content-Type': 'application/json' }
+    })
+
+    //Turn off den lights
+    fetch(hueLocalApi + apiKey + groups + denGroup + action, {
+      method: 'put',
+      body: JSON.stringify(turnOffLightBody),
+      headers: { 'Content-Type': 'application/json' }
+    })
   }
 
   res.status(200).end();
