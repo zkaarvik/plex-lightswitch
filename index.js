b2f6afc125e0bfa446ec17b0937b48e80d63a65a
Zach Kaarvik <zkaarvik@gmail.com>
2020-12-04 20:56:27 -0800

    Receive webhook and check if its from the TV

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express')
+// const bodyParser = require('body-parser');
+const fetch = require('node-fetch')
+const multer = require('multer');
+
 const app = express()
-const port = 4757
+const PORT = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const upload = multer({ dest: '/tmp/' });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+// app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+app.post('/plexwebhook',  upload.single('thumb'), (req, res, next) => {
+  var payload = JSON.parse(req.body.payload);
+  console.log('Got webhook for', payload.event);
+  // console.log(payload);
+
+  if (payload.event == 'media.play' || payload.Player.title == '55" TCL Roku TV') {
+    console.log('Media play event from the TV. Turning off the living room and den lights.')
+  }
+
+  res.status(200).end();
 })
